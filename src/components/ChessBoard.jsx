01f18cbee219f9d8f6472be5e71f1366b72d6944
9676Yaroslav9676 <yaroslavrokitskyi@gmail.com>
2023-11-02 16:27:07 +0200

    add

--- src/components/ChessBoard.jsx
+++ src/components/ChessBoard.jsx
@@ -1,4 +1,5 @@
 import "./ChessBoardModule.css";
+import Figure from "./Figure";
 import PeshkaWhite from "../images/free-icon-pawn-497273 (2).png";
 
 const ChessBoard = () => {
@@ -6,10 +7,14 @@ const ChessBoard = () => {
 
   for (let row = 0; row < 8; row++) {
     for (let col = 0; col < 8; col++) {
+      // const figure =
+      //   squares.length > 7 && squares.length < 16 ? (
+      //     <img src={PeshkaWhite}></img>
+      //   ) : null;
       const squareColor = (row + col) % 2 === 0 ? "white" : "grey";
       squares.push(
         <div key={`${row}-${col}`} className={`square ${squareColor}`}>
-          <img src={PeshkaWhite} alt="" />
+          <Figure />
         </div>
       );
     }
---
+++ src/components/Figure.jsx
@@ -0,0 +1,17 @@
+import PeshkaWhite from "../images/free-icon-pawn-497273 (2).png";
+
+const Figure = () => {
+  let figure = null;
+
+  // if (squares.obj.key > 7 && i < 16) {
+  //   figure = <img src={PeshkaWhite}></img>;
+  //   console.log(figure);
+  // } else {
+  //   figure = null;
+  //   console.log(figure);
+  // }
+
+  return <>{figure}</>;
+};
+
+export default Figure;
